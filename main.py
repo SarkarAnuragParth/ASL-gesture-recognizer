2f27fa4ceaad858bfefd82a3935b940c7964ac6f
Anurag Sarkar <101454398+SarkarAnuragParth@users.noreply.github.com>
2024-01-08 00:52:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ parser.add_argument("--mode",help="Choose between train or test mode.", default=
 parser.add_argument("--path_to_model",help="Path to store trained model. After training model will be stored in checkpoints directory if value not set",default='./checkpoints')
 parser.add_argument("--path_to_trained_model",help="Path to load trained model for testing",default='./checkpoints/ASl_Classifier')
 parser.add_argument("--epochs",help="Number of epochs to train the model",type=int)
-parser.add_argument("--val_split",help="Validation data split ratio. Only to be specified when val_path is None",type=float)
+parser.add_argument("--val_split",help="Validation data split ratio. Only to be specified when val_path is None",type=float, default=0.2)
                     
 if __name__=='__main__':    
     args=parser.parse_args()
